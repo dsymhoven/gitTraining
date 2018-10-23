33cfa4b4872d037e3813399652988870c2a297d6
dsymhove <david.symhoven@jambit.com>
2018-10-23 08:07:10 +0200

    adds sayHello method to onCreate

--- app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
+++ app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.dsymhove.gittraining
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sayHello()
+    }
+
+    private fun sayHello() {
+        Log.d("MainActivity", "Hello you")
     }
 }
