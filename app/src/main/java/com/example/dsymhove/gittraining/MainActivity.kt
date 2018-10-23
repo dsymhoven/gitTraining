d6c7c504f261e75dcb8b7e359d66e0b8fb570a37
dsymhove <david.symhoven@jambit.com>
2018-10-23 08:09:13 +0200

    adds sayPickaboo to onCreate

--- app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
+++ app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.dsymhove.gittraining
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sayPickaboo()
+    }
+
+    private fun sayPickaboo() {
+        Log.d("MainActivity", "Pickaboo")
     }
 }
