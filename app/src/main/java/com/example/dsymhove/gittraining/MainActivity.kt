3a42d3fffd922109e334e83064a370ac40aca072
dsymhove <david.symhoven@jambit.com>
2018-10-23 08:07:10 +0200

    adds sayHello method to onCreate

--- app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
+++ app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
@@ -9,7 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sayHello()
         sayPickaboo()
+    }
+
+    private fun sayHello() {
+        Log.d("MainActivity", "Hello you")
     }
 
     private fun sayPickaboo() {
