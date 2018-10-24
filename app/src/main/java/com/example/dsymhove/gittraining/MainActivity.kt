e88b24f3e2b612b2bc47db442df6342c2ed2a08f
dsymhove <david.symhoven@jambit.com>
2018-10-24 08:29:32 +0200

    adds add method to main activity again

--- app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
+++ app/src/main/java/com/example/dsymhove/gittraining/MainActivity.kt
@@ -15,4 +15,8 @@ class MainActivity : AppCompatActivity() {
     private fun sayHello() {
         Log.d("MainActivity", "Hello you")
     }
+
+    private fun add(number1: Int, number2: Int) : Int {
+        return number1 + number2
+    }
 }
